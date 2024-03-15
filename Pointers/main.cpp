e975c6a4a48823bd86d250086710ed9cfba11da6
sooshiance <s.katafract@gmail.com>
2024-03-15 16:22:09 +0330

    add pointers

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <cstdint>
+
+using namespace std;
 
 int main()
 {
+    int x{5};
+
+    int& y{x};
+
+    cout << x << endl;
+
+    cout << y << endl;
+
     return 0;
 }
